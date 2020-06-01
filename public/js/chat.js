2839181acd73b42ac4f907d7a4aa220d88ccbc31
abhinav <abhinavsingh2902@gmail.com>
2020-06-02 00:55:33 +0530

    Adding User Class and list event

--- public/js/chat.js
+++ public/js/chat.js
@@ -22,6 +22,17 @@ function scrollToBottom(){
 
 socket.on('connect',function (){
     console.log('Connected to Server');
+    var params = jQuery.deparam(window.location.search);
+    socket.emit('join',params,function(err){
+        if(err){
+            alert(err);
+            window.location.href = '/';
+        }else{
+            console.log('No error');
+        }
+
+
+    });
     // socket.emit('createEmail',{
     //     to:'abc@ex.com',
     //     text: 'Heayayda'     
@@ -34,6 +45,18 @@ socket.on('connect',function (){
 socket.on('disconnect',function (){
     console.log('Disconnected from server');
 });
+
+
+socket.on('updateUserList',function(users){
+    var ol = jQuery('<ol></ol>')
+
+    users.forEach(function(user){
+        ol.append(jQuery('<li></li>').text(user));
+
+    });
+    jQuery('#users').html(ol);
+})
+
 
 // socket.on('newEmail',function(email){
 //     console.log('New email :',email);
